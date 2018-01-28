19fd72cbff77ebd64a8b1f2a936d9a85d113259b
Ben Casalino <bencasalino@gmail.com>
2018-01-27 19:38:50 -0700

    slider

--- tower-client/src/components/Header.js
+++ tower-client/src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 export default function Header() {
-  return <header>
-      <img className="Github" src="./assets/github.png" alt="Github Logo" />
-      <img className="LogoImage" src="./assets/logo.png" alt="Logo" />
-      <span className="Logo">WHO HE PLAY FOR? </span>
-    </header>;
+  return (<header> 
+            <a href="#">
+              <img className="Github" src="./assets/github.png" alt="Github Logo" />
+            </a>
+               <img className="LogoImage" src="./assets/logo.png" alt="Logo" />
+                <span className="Logo">WHO HE PLAY FOR? </span>
+                 <div className="Container">
+                  <div className="Test"></div>
+                   <div className="Test2">  </div>
+                   <div className="Test3"></div>
+                   </div>
+       </header>);
 }
